e0af9ad1c9742c6051d726a3040e77f1fe6a749d
naveenkumar-1234 <iamnaveenkumar23@gmail.com>
2024-12-12 19:27:37 +0530

    Collections#

--- src/Collections/Main.java
+++ src/Collections/Main.java
@@ -1,9 +1,35 @@
 package Collections;
-
 import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
+
+        //List
+        List<Integer> arr = new ArrayList<>();
+        List<Integer> lin = new LinkedList<>();
+        List<Character> st = new Stack<>();
+        List<Character> v = new Vector<>();
+
+        //Set
+        Set<Integer> hs = new HashSet<>();
+        Set<String> linHs = new LinkedHashSet<>();
+
+        //Queue
+        Queue<Character> pq = new PriorityQueue<>();
+        Deque<String> ll = new LinkedList<>();
+
+        //Map
+        Map<Character,Integer> mp = new HashMap<>();
+        Map<String,Integer> ht = new Hashtable<>();
+        //Map.Entry
+
+        //Iterable
+        Iterator it = arr.iterator();
+
+        //Common methods in Collections Class
+        Collections.sort(arr);
+        Collections.shuffle(arr);
+        Collections.sort(arr,Collections.reverseOrder());
 
     }
 }
