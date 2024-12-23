258ec8920a1b0d5dc1b44ac2843c9dc3357370cd
naveenkumar-1234 <iamnaveenkumar23@gmail.com>
2024-12-23 12:52:47 +0530

    Basic recursion completed

---
+++ src/BasicRecursion/Readme.md
@@ -0,0 +1,10 @@
+# Functions and Their Complexities
+
+| Function Name                 | Time Complexity | Space Complexity |
+|-------------------------------|-----------------|------------------|
+| `sumOfFirstN_number(int n)`   | O(n)            | O(n)             |
+| `sumOfFirstN_number(int n, int sum)` | O(n)      | O(n)             |
+| `factorial(int num)`          | O(n)            | O(n)             |
+| `reverseArray(int[] arr, int i)` | O(n)         | O(n)             |
+| `isPalindrome(String str, int i)` | O(n)       | O(n)             |
+| `fibonacci(int n)`            | O(2^n)          | O(n)             |
--- src/BasicRecursion/Recursion.java
+++ src/BasicRecursion/Recursion.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class Recursion {
     static int sumOfFirstN_number(int n){
         if(n==1) return 1;
-        return n + sumOfFirstN_number(n-1);
+        return n + sumOfFirstN_number(n-1); //decrementing the numbers , if the satisfy the base condition which is n==1 return;
     }
     static void sumOfFirstN_number(int n,int sum){
         if(n<1){
@@ -14,12 +14,11 @@ public class Recursion {
         }
         sumOfFirstN_number(n-1,sum+n);
     }
-
     static int factorial(int num){
         if(num==1) return 1;
-        return num * factorial(num-1);
+        return num * factorial(num-1);      //if n=5 it performs 5*4*3*2*1
     }
-    static void swap(int[] arr,int i,int len){
+    static void swap(int[] arr,int i,int len){   // it performs normal swapping by using one temp variable
         int temp=arr[i];
         arr[i]=arr[len-i-1];
         arr[len-i-1]=temp;
@@ -27,12 +26,28 @@ public class Recursion {
     static void reverseArray(int[] arr,int i){
         int len=arr.length;
         if(i>=len/2) return;
-        swap(arr,i,len);
+        swap(arr,i,len);      // passing array ref , pointer i and length to swap
         reverseArray(arr,i+1);
     }
+    static boolean compare(String str,int i){
+        int len=str.length();     // we find length to reduce another pointer
+        if(str.charAt(i)==str.charAt(len-i-1)) return true;  //we're using len-i-1 instead of another pointer variable. Eg:j=len-1; j--; in every iteration
+        return false;
+    }
+    static boolean isPalindrome(String str,int i){
+        if(i >= (str.length()-1)/2) return true;
+        if(compare(str,i)) return compare(str,i+1);      // if it fails then it couldn't be Palindrome
+        return false;
+           }
+    static int fibonacci(int n){
+                if(n==0) return 0;
+                if(n==1) return 1;
+                return fibonacci(n-1) + fibonacci(n-2);   // its take O(2pow(n)) as Time complexity. It exponential growth because of 2 func calls
+           }
     public static void main(String[] args) {
         int[] arr={6,3,6,3,4,6,79};
         reverseArray(arr,0);
-
+        System.out.println(isPalindrome("1211",0));
+//        System.out.println(fibonacci(0));
     }
 }
